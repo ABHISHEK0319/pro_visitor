0913d5918912b1284b3863fd904fcd3870a530ce
Nitesh Kumar <niteshkumarrao19@gmail.com>
2022-09-14 17:21:26 +0530

    todo before db

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'Screens/to_do.dart';
 import 'Screens/appointments.dart';
 import 'Screens/todo_models/todo_list_page.dart';
 
 void main() {
-  //runApp(const ToDo_Page());
-  runApp(const Appo_Page());
+  runApp(const ToDo_Page());
+  //runApp(const Appo_Page());
   //runApp(const contactpage());
 }
